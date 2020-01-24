537dd85492b01f3d74a114b1d73641f9301fc2ab
Kerndog73 <kerndog73@gmail.com>
2020-01-24 13:29:30 +1030

    Setup project

---
+++ Silver Lobster/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  Silver Lobster
+//
+//  Created by Indi Kernick on 24/1/20.
+//  Copyright © 2020 Indi Kernick. All rights reserved.
+//
+
+#include <cassert>
+#include <SDL2/SDL.h>
+
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  
+  SDL_Window *window = SDL_CreateWindow(
+    "Silver Lobster",
+    SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    1280, 720,
+    SDL_WINDOW_SHOWN
+  );
+  assert(window);
+  
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+    window,
+    -1,
+    SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+  );
+  assert(renderer);
+  
+  bool running = true;
+  do {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        running = false;
+      }
+    }
+    
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  } while (running);
+  
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
